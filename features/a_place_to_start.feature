c387fc1e2c9a6b3fb1cc7f3e48e26cc148aea96e
jamesmartin <jimmymartin@gmail.com>
2010-08-19 09:52:27 +1000

    Feature: a login form for Twitter authentication

--- features/a_place_to_start.feature
+++ features/a_place_to_start.feature
@@ -8,3 +8,11 @@ Feature: a place to start
     Given I have a web browser 
     When I point my browser to the home page
     Then I should see "Welcome to Missed Links!"
+
+  Scenario: a 'login to Twitter' form
+    Given I have a web browser
+    And I am not authenticated with Twitter
+    When I point my browser to the home page
+    Then I should see an input for Twitter username
+    And I should see an input for Twitter password
+    And I should see a "Login" button
--- features/step_definitions/missedlinks_steps.rb
+++ features/step_definitions/missedlinks_steps.rb
@@ -4,10 +4,25 @@ Given /^I have a web browser$/ do
   @browser = Watir::Safari.new
 end
 
+Given /I am not authenticated with Twitter$/ do
+end
+
 When /I point my browser to the home page/ do
   @browser.goto("http://localhost:9393")
 end
 
 Then /I should see "([^\"]*)"$/ do |greeting|
   @browser.contains_text(greeting).should be_true
+end
+
+Then /^I should see an input for Twitter username$/ do
+  @browser.text_field(:name, 'twitter_username').exists?.should be_true
+end
+
+Then /^I should see an input for Twitter password$/ do
+  @browser.text_field(:name, 'twitter_password').exists?.should be_true
+end
+
+Then /^I should see a "([^\"]*)" button$/ do |button_name|
+  @browser.text_field(:name, button_name).exists?.should be_true
 end
