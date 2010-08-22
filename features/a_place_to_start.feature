ccc1cd1965d6243b94fb9d384622d612f00c8f9c
jamesmartin <jimmymartin@gmail.com>
2010-08-23 08:24:13 +1000

    Reworded some of the test language. Still not happy with it

--- features/a_place_to_start.feature
+++ features/a_place_to_start.feature
@@ -1,7 +1,10 @@
-Feature: basic web application behaviour 
+Feature: basic web application OAuth dance behaviour 
 
-  Scenario: Web application displays a home page and option to authenticate 
-    Given that the authentication service is running 
+  Scenario: Web application allows the web user to authenticate with a third party 
+    Given that the authentication provider service is running 
       And the web application has started
     When the user browses to the home page
-    Then the user is able to attempt authentication
+    Then the web application prompts the user to authenticate with the authentication provider service 
+
+    When the user authenticates successfully with the authentication provider service 
+    Then the web application shows that the web user has authenticated successfully 
--- features/step_definitions/all_steps.rb
+++ features/step_definitions/all_steps.rb
@@ -1,36 +1,4 @@
-require 'safariwatir'
 
-Given /^I have a web browser$/ do
-  @browser = Watir::Safari.new
+Given /^that the authentication provider service is running$/ do
+  @authentication_provider = FakeAuthenciationProviderService.new
 end
-
-When /I point my browser to the home page/ do
-  @browser.goto("http://localhost:9393")
-end
-
-Given /I am at the home page/ do
-  Given "I have a web browser"
-  When "I point my browser to the home page"
-end
-
-Given /I am not authenticated with Twitter$/ do
-  #pending "figure out how to fake a twitter OAuth service"
-end
-
-When /I click the "([^\"]*)" link$/ do |link_text|
-  @browser.link(:text, link_text).click
-end
-
-Then /I should see "([^\"]*)"$/ do |greeting|
-  @browser.contains_text(greeting).should be_true
-end
-
-Then /^I should see a "([^\"]*)" link$/ do |link_text|
-  @browser.link(:id, 'authenticate').exists?.should be_true
-  @browser.link(:text, link_text).exists?.should be_true
-end
-
-Then /^I should be redirected to the "([^\"]*)" page$/ do |page_text|
-  @browser.contains_text(page_text).should be_true
-end
-
